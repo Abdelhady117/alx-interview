b4c65a052cad8bad448271fbc13f848822e52a32
Abdelhady117 <111305193+Abdelhady117@users.noreply.github.com>
2025-02-01 04:53:12 +0200

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -19,26 +19,26 @@ def isWinner(x, nums):
                 for j in range(i * i, n + 1, i):
                     primes[j] = False
         return primes
-    
+
     if x < 1 or not nums:
         return None
-    
+
     max_n = max(nums)
     prime_flags = sieve(max_n)
     prime_counts = [0] * (max_n + 1)
-    
+
     for i in range(1, max_n + 1):
         prime_counts[i] = prime_counts[i - 1] + (1 if prime_flags[i] else 0)
-    
+
     maria_wins = 0
     ben_wins = 0
-    
+
     for n in nums:
         if prime_counts[n] % 2 == 1:
             maria_wins += 1
         else:
             ben_wins += 1
-    
+
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
